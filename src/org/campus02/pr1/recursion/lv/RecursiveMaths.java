a48bf1456834baaebb2c8a2e7851635988b90bf4
Daniel Pausackerl <daniel.pausackerl@edu.campus02.at>
2016-02-09 20:53:34 +0100

    upload

---
+++ src/org/campus02/pr1/recursion/lv/RecursiveMaths.java
@@ -0,0 +1,27 @@
+package org.campus02.pr1.recursion.lv;
+
+public class RecursiveMaths {
+
+	public static int sum(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		return sum(n - 1) + n;
+	}
+
+	public static int fibonacci(int n) {
+		if (n <= 2) { // oder auch if (n == 1 || n == 2) oder n == 1 || 2
+						// möglich
+			return 1;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+	public static long factorial(int n) {
+		if (n <= 1) {
+			return 1;
+		}
+		return n * factorial(n - 1);
+	}
+
+}
